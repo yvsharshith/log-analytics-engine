db6d5d04f831b63d04931d8c69c803c27887c583
harshith <harshith7422@gmail.com>
2026-01-05 19:06:51 +0530

    changed the error in regex

--- ingestion/parser.py
+++ ingestion/parser.py
@@ -1,7 +1,7 @@
 import re
 
 def parse_log_line(line):
-    log_pattern = r'^(?P<timestamp>\S+ \S) (?P<level>\S+) (?P<service>\S+) (?P<message>.\S)$'
+    log_pattern = r'^(?P<timestamp>\S+\s+\S+) (?P<level>\S+) (?P<service>\S+) (?P<message>.*)$'
     match = re.match(log_pattern, line)
     if match:
         return match.groupdict()
