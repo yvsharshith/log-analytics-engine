6c0e71f1ef045cbca4319591987a6d51f1acf042
harshith <harshith7422@gmail.com>
2026-01-06 14:57:45 +0530

    created a pipeline and updated main for milestone-2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from config.dask_config import start_dask
-from ingestion.loader import load_logs
-from ingestion.parser import parse_log_line
+from processing.pipeline import build_pipeline
 import time
 
 def main():
@@ -9,26 +8,17 @@ def main():
     print(f"Dashboard link: {client.dashboard_link}")
     print("\n" + "="*50)
     
-    bag = load_logs("/Users/harshithyvs/Desktop/log-analytics-engine/data/sample_log.log")
-    parsed = bag.map(parse_log_line).filter(lambda x: x is not None)
+    start = time.time()
+    log_df = build_pipeline("data/sample_log.log")
+    print("start time", start)
     
-    result = parsed.compute()
-    print("\nParsed Logs:")
-    for log in result:
-        print(log)
+    total_logs = log_df.count().compute()
+    end = time.time()
     
-    print("\n"+"="*50)
-    print("Dashboard is running at: http://127.0.0.1:8790/status")
-    print("Press Ctrl+C to exit...")
-    print("="*50)
+    print("total logs parssed:", total_logs)
+    print("time taken", end)
     
-    #keep the script runinng so the dashboard stays active
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("\n\nShutting down Dask cluster...")
-        client.close()
-
+    input("press enter....")
+    
 if __name__ == "__main__":
     main()
